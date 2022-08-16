9cb0a72fafe39893a181f04b63b091409b1963c7
jonasngs <jonasngs30@gmail.com>
2022-08-16 17:17:02 +0800

    serve static content

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 // Shorten long url
