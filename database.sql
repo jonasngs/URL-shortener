6d94fe7aa3b9c734666b469071aabe371aa94f28
jonasngs <jonasngs30@gmail.com>
2022-08-16 16:02:27 +0800

    Add configs for heroku

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE url_shortener_db;
 
 CREATE TABLE url_tab(
   id SERIAL PRIMARY KEY,
-  long_url VARCHAR(200) NOT NULL UNIQUE,
-  short_url VARCHAR(100) NOT NULL UNIQUE,
+  long_url VARCHAR(2400) NOT NULL UNIQUE,
+  short_url VARCHAR(160) NOT NULL UNIQUE,
   created_at BIGINT NOT NULL
 );
